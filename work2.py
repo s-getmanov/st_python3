42aec14038f77112d1bd81ac1fc19e219a65490e
Sergey <you@example.com>
2023-05-31 21:08:46 +0300

    Задача 2

---
+++ work2.py
@@ -0,0 +1,25 @@
+#ChatGPT говорит нам, что этапов было 10, и мы ему поверим:)
+# 1. Sahelanthropus tchadensis
+# 2. Orrorin tugenensis
+# 3. Ardipithecus ramidus
+# 4. Australopithecus anamensis
+# 5. Australopithecus africanus
+# 6. Homo habilis
+# 7. Homo erectus
+# 8. Homo heidelbergensis
+# 9. Homo neanderthalensis
+# 10. Homo sapiens (modern humans)
+
+#Так как циклы мы еще не проходили, а количество фиксированное, то запихнем все в 10 переменных.
+a1 = input("Введите название периода 1: ")
+a2 = input("Введите название периода 2: ")
+a3 = input("Введите название периода 3: ")
+a4 = input("Введите название периода 4: ")
+a5 = input("Введите название периода 5: ")
+a6 = input("Введите название периода 6: ")
+a7 = input("Введите название периода 7: ")
+a8 = input("Введите название периода 8: ")
+a9 = input("Введите название периода 9: ")
+a10 = input("Введите название периода 10: ")
+
+print(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10, sep=" => ")
