b740876fd88dc48b3c03698ba32d7b932f746f52
Sergey <you@example.com>
2023-05-31 20:33:57 +0300

    Задача 1

--- work1.py
+++ work1.py
@@ -1,1 +1,8 @@
-print("Hello!")
+# Ссылка на github
+# https://github.com/s-getmanov/st_python3.git
+
+type_ = input("Введите вид питомца:")
+name_ = input("Введите кличку питомца:")
+age_ = input("Введите возраст питомца:")
+
+print("Это " +type_+ " по кличке " +name_+ ". Возраст " +age_+" года.")
